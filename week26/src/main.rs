770eec58ad0926b20312e8619fd3155331043a59
Anilb08 <anilbhoi7740@gmail.com>
2025-12-04 16:22:06 +0530

    basic rust

--- week26/src/main.rs
+++ week26/src/main.rs
@@ -1,28 +1,11 @@
 fn main() {
-    // println!("Hello, world!");
-    // let ans = sum(1,7);
-    // println!(" {}", ans);
-    // let name = "anil"; // fixed string 
-    // let lastName = String::from("Bhoi");
-    // println!("firstName -> {}", name);
-    // println!("Lastname -> {}", lastName);
+ let x = 1;
+ println!("{}", x);
 
-    let even = is_even(4);
-    
-    if(even){
-        println!("{}: this is even", even);
-    } else{
-         println!("{}: this is odd", even);
-    }
-}
+ let  mut num: i8 =  124;
 
-// fn sum(a:u32, b:u32)->u32{
-//     return a+b;
-// }
-
-
-
-
-fn is_even(x:u32)-> bool{
-    return x % 2 == 0;
+ for i in 0..100{
+    num += 127;
+ }
+ print!("Number: {}", num)
 }
