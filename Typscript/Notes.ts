312e68877e97fb1fdd793b10b6dc28bcb95c0502
Anilb08 <anilbhoi7740@gmail.com>
2025-12-05 14:49:12 +0530

    basic types of Typescript

--- Typscript/Notes.ts
+++ Typscript/Notes.ts
@@ -12,3 +12,62 @@
 
 
 // Typescript being converted into JavaScript means it runs anywhere that Javascript runs!
+
+
+
+
+// # Typescript simple types  ->>
+
+     // The most basic types in Typescript are called primitives.
+
+     // most often five primitive types you'll use
+
+         // 1. Boolean 
+              let isActive:boolean = true;
+
+              let hasPermission = false;  // Typescript infers 'boolean' type 
+
+
+
+        // 2. Number
+        
+              // Represents both integers and floating-point numbers.
+              // ts uses the same number type for all numeric values.
+
+
+              let decimal:number = 6;
+              let hex:number = 0xf00d; // Hexadecimal
+              let binary:number = 0b1010; // Binary
+              let octal: number = 0o744;  // Octal
+              let float: number = 3.14;  // Floating point
+
+
+            
+
+        // 3.String
+          
+         // Represents text data.
+         //Can use single quotes ('), double quotes ("), or backticks (`) for template literals.
+
+         let color: string = "blue";
+         let fullName: string = 'John Doe';
+
+
+         
+// Basic explicit types 
+
+    let greeting: string = "Hello, Typescript";
+
+    function greet(name: string):string {
+        return `Hello, ${name}!`;
+    }
+
+    greet("Anil");
+
+    
+    
+
+    
+
+
+
