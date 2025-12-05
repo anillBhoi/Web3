f6ee0cf586a8b0f6c1f3c688226f105eeec487fc
Anilb08 <anilbhoi7740@gmail.com>
2025-12-05 08:52:44 +0530

    typescript learning

---
+++ Typscript/Notes.ts
@@ -0,0 +1,14 @@
+//Typscript being a syntactic superset means it shares the same
+// base syntax as JavaScript, but adds something to it.
+
+
+// For example, TypeScript will report an error when passing 
+// a string into a function that expects a number.
+// JavaScript will not.
+
+
+// Typscript uses compile time type checking.
+// Which means it checks if the specified types match before running the code, not while running the code.
+
+
+// Typescript being converted into JavaScript means it runs anywhere that Javascript runs!
