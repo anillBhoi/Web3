2ec5b7bbc8cac34ae6a43a1795fb8f69b9686e4b
Anilb08 <anilbhoi7740@gmail.com>
2025-12-17 09:02:43 +0530

    completed the basic conditions and loops

--- Rust/RustPractice/src/main.rs
+++ Rust/RustPractice/src/main.rs
@@ -52,4 +52,36 @@ fn main() {
     xs.push(4);
     println!("{}", xs.len());
 
+
+    // ## Conditionals , Looops  
+
+    let x = 99; 
+    let is_even = is_even(x); 
+
+    if is_even {
+        println!("{} is even ", x);
+    } else {
+        println!("{} is odd ", x);
+    }
+
+
+    // Looop 
+    let str = String::from("Anil Bhoi");
+    println!("first name {}", get_first_name(str))
+
+}
+
+pub fn is_even(x: i32) -> bool {
+    return x % 2 == 0;
+}
+
+pub fn get_first_name(str: String) -> String {
+    let mut first_name = String::from("");
+    for c in str.chars() {
+        if c == ' '{
+            break
+    }
+    first_name.push(c);
+}
+return first_name;
 }
